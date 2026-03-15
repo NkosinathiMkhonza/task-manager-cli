7a9b5d73431664d150a53250fcf1b93a31339022
Nkosinathi <nkosimkhonza5@gmail.com>
2026-03-15 20:28:23 +0200

    feat: initialize SQLite database with tasks table

---
+++ manage.py
@@ -0,0 +1,28 @@
+import sqlite3
+import os
+
+DR_PATH = os.path.join(os.path.dirname(__file__), "task.db")
+
+
+def get_connection():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def init_db():
+    conn = get_connection()
+    conn.execute("""
+        CREATE TABLE IF NOT EXISTS task (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            description TEXT,
+            completed BOOLEAN DEFAULT 0
+            )
+        """)
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    init_db()
+    print("Database initialized successfully.")
