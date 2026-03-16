fe705a34c24cabb158dee1bbaf030bc9b64b0547
Nkosinathi <nkosimkhonza5@gmail.com>
2026-03-16 14:47:41 +0200

    feat: add Render deployment config

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def api_delete_task(task_id):
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
